b1cc755c27b51917804810d6df7549d7ffbfc533
Dennis.westphal <mail@denniswestphal.de>
2018-06-05 18:02:07 +0200

    use mist/metamask again

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -70,7 +70,6 @@ window.App = {
 };
 
 window.addEventListener('load', function() {
-	/*
 	// Checking if Web3 has been injected by the browser (Mist/MetaMask)
 	if (typeof web3 !== 'undefined') {
 		// Use Mist/MetaMask's provider
@@ -79,10 +78,9 @@ window.addEventListener('load', function() {
 		console.warn(
 				'No web3 detected. Falling back to http://127.0.0.1:7545. You should remove this fallback when you deploy live, as it\'s inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask');
 		// fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-	*/
-	window.web3 = new Web3(
-			new Web3.providers.HttpProvider('http://127.0.0.1:7545'));
-	//}
+		window.web3 = new Web3(
+				new Web3.providers.HttpProvider('http://127.0.0.1:7545'));
+	}
 
 	App.start();
 });
